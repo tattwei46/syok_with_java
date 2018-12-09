023227bb27507e5cf48d07e37c7140b91ee3295e
DavidCheah <tattwei46@gmail.com>
2018-12-09 10:05:20 +0800

    Initial commit

---
+++ src/syok_with_java/HelloWorld.java
@@ -0,0 +1,17 @@
+package syok_with_java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Syok with Java");
+		
+		int x = 10;
+		int y = 3;
+		int sum = x + y;
+		
+		System.out.println("sum is " + sum);
+		
+
+	}
+
+}
